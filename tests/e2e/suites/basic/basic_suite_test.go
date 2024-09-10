84834159f69a65582c9e07dfcc134efb8f62e736
Matías Charrière <matias@giantswarm.io>
2024-09-10 22:35:18 +0200

    Add basic test checking if the HelmRelease is deployed (#221)
    * Add basic test checking if the HelmRelease is deployed
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * update clustertest
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * rework helmrelease check
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * go mod tidy
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -1,14 +1,21 @@
 package basic
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 	"time"
 
-	// . "github.com/onsi/ginkgo/v2"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
+	helmv2beta1 "github.com/fluxcd/helm-controller/api/v2beta1"
 	"github.com/giantswarm/apptest-framework/pkg/config"
+	"github.com/giantswarm/apptest-framework/pkg/state"
 	"github.com/giantswarm/apptest-framework/pkg/suite"
+	"github.com/giantswarm/clustertest/pkg/logger"
+
+	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
@@ -31,7 +38,36 @@ func TestBasic(t *testing.T) {
 			// E.g. ensure that the initial install has completed and has settled before upgrading
 		}).
 		Tests(func() {
-			// we don't do anything here
+			It("should deploy the HelmRelease", func() {
+
+				Eventually(func() (bool, error) {
+					appNamespace := state.GetCluster().Organization.GetNamespace()
+					appName := fmt.Sprintf("%s-cilium", state.GetCluster().Name)
+
+					mcKubeClient := state.GetFramework().MC()
+
+					logger.Log("HelmRelease: %s/%s", appNamespace, appName)
+
+					release := &helmv2beta1.HelmRelease{}
+					err := mcKubeClient.Get(state.GetContext(), types.NamespacedName{Name: appName, Namespace: appNamespace}, release)
+					if err != nil {
+						return false, err
+					}
+
+					for _, c := range release.Status.Conditions {
+						if c.Type == "Ready" && c.Status == "True" {
+							return true, nil
+						} else {
+							return false, errors.New(fmt.Sprintf("HelmRelease not ready [%s]: %s", c.Reason, c.Message))
+						}
+					}
+
+					return false, errors.New("HelmRelease not ready")
+				}).
+					WithTimeout(5 * time.Minute).
+					WithPolling(15 * time.Second).
+					Should(BeTrue())
+			})
 		}).
 		Run(t, "Basic Test")
 }
