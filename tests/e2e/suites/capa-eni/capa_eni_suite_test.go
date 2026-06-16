bf35e84ba4635d9877edcf817fa0911f266f292d
Gerald Pape <gerald@giantswarm.io>
2026-06-16 14:47:46 +0200

    Update CI and test files

--- tests/e2e/suites/capa-eni/capa_eni_suite_test.go
+++ tests/e2e/suites/capa-eni/capa_eni_suite_test.go
@@ -10,10 +10,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/giantswarm/apptest-framework/v3/pkg/state"
-	"github.com/giantswarm/apptest-framework/v3/pkg/suite"
-	"github.com/giantswarm/clustertest/v3/pkg/logger"
-	"github.com/giantswarm/clustertest/v3/pkg/wait"
+	"github.com/giantswarm/apptest-framework/v5/pkg/state"
+	"github.com/giantswarm/apptest-framework/v5/pkg/suite"
+	"github.com/giantswarm/clustertest/v5/pkg/logger"
+	"github.com/giantswarm/clustertest/v5/pkg/wait"
 
 	"github.com/giantswarm/cilium-app/tests/e2e/internal/connectivity"
 	"github.com/giantswarm/cilium-app/tests/e2e/internal/metrics"
